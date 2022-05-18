65d5e4060799ac182a3ecb53e8b0c60e71d4c749
armin-bkh <arminbkh0921@yahoo.com>
2022-05-19 02:05:13 +0430

    ScrollView pull to refresh

--- src/components/RefreshControl/RefreshControl.tsx
+++ src/components/RefreshControl/RefreshControl.tsx
@@ -1,4 +1,24 @@
+import globalStyles from 'constants/styles';
 import React from 'react';
-import {RefreshControl} from 'react-native';
+import {RefreshControl, ScrollView} from 'react-native';
 
-export default RefreshControl;
+// export  RefreshControl;
+
+interface pullToRefreshControlProps {
+  profileLoading: boolean;
+  refetching: boolean;
+  onRefresh: () => Promise<any>;
+  children: React.ReactChild;
+}
+
+export default function PullToRefreshControl(props: pullToRefreshControlProps) {
+  const {profileLoading, refetching, onRefresh, children} = props;
+  return (
+    <ScrollView
+      style={[globalStyles.screenView, globalStyles.fill]}
+      refreshControl={<RefreshControl refreshing={profileLoading || refetching} onRefresh={onRefresh} />}
+    >
+      {children}
+    </ScrollView>
+  );
+}
--- src/components/RefreshControl/RefreshControl.web.tsx
+++ src/components/RefreshControl/RefreshControl.web.tsx
@@ -1,4 +1,26 @@
+import globalStyles from 'constants/styles';
 import React from 'react';
+import {ScrollView, View} from 'react-native';
 import {RefreshControl} from 'react-native-web-refresh-control';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 
-export default RefreshControl;
+// export RefreshControl;
+
+interface pullToRefreshControlProps {
+  profileLoading: boolean;
+  refetching: boolean;
+  onRefresh: () => Promise<any>;
+  children: React.ReactChild;
+}
+
+export default function PullToRefreshControl(props: pullToRefreshControlProps) {
+  const {onRefresh, children} = props;
+
+  return (
+    <ScrollView style={[globalStyles.screenView, globalStyles.fill]}>
+      <PullToRefresh maxPullDownDistance={150} onRefresh={onRefresh}>
+        <>{children}</>
+      </PullToRefresh>
+    </ScrollView>
+  );
+}
